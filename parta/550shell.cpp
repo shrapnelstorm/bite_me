d6053fc4ca15f66371d3d15b3b3465f2e0ec4d38
sivanr <sivanr@cs>
2014-01-11 16:23:55 -0800

    setup_pip and init_child functions added

--- parta/550shell.cpp
+++ parta/550shell.cpp
@@ -3,3 +3,33 @@
 int main() {
 	std::cout << "hello world\n" ;
 }
+
+int setup_pip(int &cur_output, int &next_input){
+  int pip[2];
+  int result;
+  result = pipe(pip);
+  if(result == -1){
+    perror("Unable to create a pipe");
+    exit(1);
+  }
+  else {
+    cur_output = pip[1];
+    next_input = pip[0];
+  }
+  return 1;
+}
+
+int init_child(char* cmd, int input, int output){
+  int result1,result2;
+  result1 = dup2(input,0);
+  if(result1 == -1){
+    perror("dup2 failed");
+    exit(1);
+  }
+  result2 = dup2(output,1);
+  if(result2 == -1){
+    perror("dup2 failed");
+    exit(1);
+  }
+  execve(cmd,NULL,NULL);
+}
