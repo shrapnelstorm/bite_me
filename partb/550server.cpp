f9225ede4c080079c34ef7a09de3964666837b4f
Armando <ajdt@skynet.gov>
2014-01-14 13:09:45 -0800

    code to create tcp socket

--- partb/550server.cpp
+++ partb/550server.cpp
@@ -9,7 +9,10 @@
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <netinet/in.h>
+#include <netdb.h>
 
+#include <iostream>
+void server_listen(int socket) ;
 
 int main(int argc, char** argv) {
 	
@@ -18,26 +21,53 @@ int main(int argc, char** argv) {
 		printf("usage: ./550server <ip_addr> <port>") ;
 	}
 
-	// create listen socket
-	int	server_socket = socket(AF_INET, SOCK_STREAM, 0) ;
-	if (server_socket < 0) {
-		perror("could not create socket") ;
+	// setup server address info
+	struct addrinfo server_info, *server_info_list ;
+	memset(&server_info, 0, sizeof server_info) ;
+	server_info.ai_family	= AF_UNSPEC ;
+	server_info.ai_socktype = SOCK_STREAM ;
+
+	// change argv[1] to NULL to automatically use localhost
+	int status = getaddrinfo(argv[1], argv[2], &server_info, &server_info_list) ;
+	if (status != 0) std::cout << "getaddrinfo() failed: " << gai_strerror(status) ;
+
+
+	// create the socket
+	int server_socket =	socket(server_info_list->ai_family, server_info_list->ai_socktype, server_info_list->ai_protocol) ;
+	if (server_socket == -1) {
+		std::cout << "error creating server socket" ;
+		exit(1) ;
+	}
+	
+	// bind socket to port
+	int yes=1 ;
+	status = setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int) );
+	status = bind(server_socket, server_info_list->ai_addr, server_info_list->ai_addrlen) ;
+
+	if (status == -1) {
+		std::cout << "bind error" << std::endl ;
+	}
+
+
+	// ensure the parameters to listen() are correct
+	if ( listen(server_socket, 6) == -1)  {// can buffer 6 connections
+		std::cout << "listen error" << std::endl ;
 		exit(1) ;
 	}
 
-	struct sockaddr_in addr; 	// internet socket address data structure
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(atoi(argv[1])); // byte order is significant
-	// does this need to be fixed???
-	addr.sin_addr.s_addr = INADDR_ANY; // listen to all interfaces
 
-	int yes=1;
-	if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-		perror("setsockopt failed");
-		exit(1);
-	}
-
+	// free the server_info_list 
 	return 0 ;
+}
+
+
+void server_listen(int socket) {
+	// setup the select data structures that are necessary
+
+
+	// listen for connections & process existing conn.
+	while (true) {
+	}
 }
 
 
